d2dc17c0142da35b9cc33dfda78b288d4959a729
Likun Du <begdor@users.noreply.github.com>
2016-05-31 14:59:24 -0700

    extract movie data to item

--- scrapy/racoon/spiders/imdb_spider.py
+++ scrapy/racoon/spiders/imdb_spider.py
@@ -9,11 +9,11 @@ class RacoonSpider(scrapy.Spider):
     start_urls = []
 
     def __init__(self):
-        for year in range(1990,2017):
+        for year in range(1990,1991):
             self.start_urls.append('http://www.imdb.com/search/title?year='+str(year)+','+str(year)+'&title_type=feature&sort=moviemeter,asc')
 
     # MARK: crawl and extract movie data and handling pagination
-    def parse_movie(self, response):
+    def parse_movies(self, response):
         #extract movie data:
         for movie in response.xpath("//table[@class='results']/tr"):
             item = RacoonItem()
@@ -22,22 +22,28 @@ class RacoonSpider(scrapy.Spider):
             item['year'] = movie.xpath("td[@class='title']/span[@class='year_type']/text()").extract()
             item['userRating'] = movie.xpath("td[@class='title']/div[@class='usr_rating']/span[@class='value'/text()").extract()
             item['outline'] = movie.xpath("td[@class='title']/span[@class='outline']/text()").extract()
+
+            #extract director and cast information
             credit = movie.xpath("td[@class='title']/span[@class='credit']/text()").extract()
             creditList = credit.split("Dir: ")
             dirList = creditList[1].split(',')
             castList = creditList[2].split('With: ')[1].split(',')
 
             for dirA in dirList:
-                if(char)
+                start = dirA.find('>')
+                end = dirA.find('<',start)
+                item['director'].append(dirA[start+1:end])
+            for castA in castList:
+                start = castA.find('>')
+                end = castA.find('<',start)
+                item['cast'].append(castA[start+1:end])
 
-
-
-
+            yield item
 
         #follwing the link to the next page until it doesn't find one
-        next_page = response.css(".pagination > a[text~="Next"]::attr('href')" ):
+        next_page = response.xpath("//span[@class='pagination']/a[contains(text(),'Next')]/@href")
         # use urljoin method to yield new requests to be sent later
         if next_page:
-            url = response.urljoin(href.extract())
+            url = response.urljoin(next_page[0].extract())
             #register as callback method parse_dir_contents()
-            yield scrapy.Request(url, callback=self.parse_movie)
+            yield scrapy.Request(url, callback=self.parse_movies)
